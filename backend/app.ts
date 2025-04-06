a69ee9587e91417d3be75a9a316dc42f9665a093
Damon Thomas <damon22t@gmail.com>
2025-04-05 22:40:04 -0400

    cors basic

--- backend/app.ts
+++ backend/app.ts
@@ -12,12 +12,7 @@ import profileRouter from "./routes/profile.js";
 
 const app: express.Express = express();
 
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
