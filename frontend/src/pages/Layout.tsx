0635f23439bb6a257d41199ad5473b101758970c
Damon Thomas <damon22t@gmail.com>
2025-04-06 10:05:57 -0400

    feat: add @vercel/analytics dependency and integrate analytics component in Layout

--- frontend/src/pages/Layout.tsx
+++ frontend/src/pages/Layout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Header from "./header/Header";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function Layout() {
   return (
     <div className="layout h-screen w-screen overflow-hidden flex flex-col ">
       <Header />
       <Outlet />
+      <Analytics />
     </div>
   );
 }
